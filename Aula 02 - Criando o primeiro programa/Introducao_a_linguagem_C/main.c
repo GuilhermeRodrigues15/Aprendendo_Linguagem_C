78a1dad8d634dcdd1d5e5f6e089d014310d76aa2
Guilherme Rodrigues <oguilhermerodrigues2002@gmail.com>
2022-05-14 23:07:01 -0300

    Criando o primeiro programa e C

---
+++ Aula 02 - Criando o primeiro programa/Introducao_a_linguagem_C/main.c
@@ -0,0 +1,17 @@
+// Iniciando nossa jornada em C.
+
+// Comentário de uma linha.
+
+/* Comentário
+   de
+   varias
+   linhas.
+*/
+
+#include <stdio.h> // Diretivas INCLUDE, extensão ".h" significa (Header - Cabeçalho).
+#include <stdlib.h>
+
+void main() // Declaração da função main (Principal), a palavra "void" indica o tipo de retorno da funçao que será vazio.
+{
+    printf("Bem vindo ao curso de linguagem C!!!\n"); // "printf" função que gera uma saída de dados, "/n" insere uma linha abaixo.
+}
